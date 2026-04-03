327a5d73de0543c7897a7c001a7e06ba1a33b397
root <root@SD-20210212DPTH.localdomain>
2026-04-03 23:48:26 +0800

    [fix]用户提交/取消按钮放置右下;单选题水平放置

--- edc/urls.py
+++ edc/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('czy-edc-2026/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', RedirectView.as_view(url='login/', permanent=False), name='home'),
 ]
